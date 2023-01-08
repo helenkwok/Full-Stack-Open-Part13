82c5710b9db6fa19c7f7216a8f28b7ae06692532
helenkwok <107752876+helenkwok@users.noreply.github.com>
2023-01-08 17:34:54 +1030

    13.3: Print blogs on command-line

--- bloglist-app/index.js
+++ bloglist-app/index.js
@@ -1,13 +1,45 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    default: 0
+  },
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
 app.get('/api/blogs', async (req,res) => {
-  const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-  res.json(blogs)
+  const blogs = await Blog.findAll()
+  blogs.forEach(blog => {
+    console.log(`${blog.author}: '${blog.title}, ${blog.likes} likes'`)
+  })
 })
 
 const PORT = process.env.PORT || 3001
