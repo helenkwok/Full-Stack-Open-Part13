f9dd0245310b11c878a6354ccd3eff5632b45b39
helenkwok <107752876+helenkwok@users.noreply.github.com>
2023-01-08 17:19:00 +1030

    13.2: Create a blogs table

---
+++ bloglist-app/commands.sql
@@ -0,0 +1,11 @@
+CREATE TABLE blogs (
+    id SERIAL PRIMARY KEY,
+    author text,
+    url text NOT NULL,
+    title text NOT NULL,
+    likes integer DEFAULT 0
+);
+
+insert into blogs (author, url, title) values ('Dan Abramov','https://overreacted.io/on-let-vs-const','On let vs const');
+
+insert into blogs (author, url, title) values ('Laurenz Albe','https://www.cybertec-postgresql.com/en/gaps-in-sequences-postgresql','Gaps in sequences in PostgreSQL');
