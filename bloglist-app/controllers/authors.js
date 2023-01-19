361e1ba572c8d98238eed52581f955c3c70ab27f
helenkwok <107752876+helenkwok@users.noreply.github.com>
2023-01-19 15:47:03 +1030

    13.16: Returns number of blogs for each author and total number of likes

---
+++ bloglist-app/controllers/authors.js
@@ -0,0 +1,19 @@
+const authorRouter = require('express').Router()
+const { Blog, User } = require('../models')
+const { Op } = require('sequelize')
+const { sequelize } = require('../util/db')
+
+authorRouter.get('/', async (req, res) => {
+    authors = await Blog.findAll({
+      order: [['likes', 'DESC']],
+      attributes: [
+          'author',
+          [sequelize.fn('COUNT', sequelize.col('author')), 'articles'],
+          [sequelize.fn('SUM', sequelize.col('likes')), 'likes']
+      ],
+      group: 'author'
+    });
+    res.json(authors)
+})
+
+module.exports = authorRouter
